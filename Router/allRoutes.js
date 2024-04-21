afcf948f4c6c5ce88e0a24be7fa6c98f7b1fdf86
SoufianeHakkou <soufiane802@gmail.com>
2024-04-22 01:01:53 +0200

    nav bar functioning

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -1,7 +1,12 @@
 import Route from "./Route.js";
 
 //Définir ici vos routes
-export const allRoutes = [new Route("/", "Accueil", "/pages/home.html")];
+export const allRoutes = [
+  new Route("/", "Accueil", "/pages/home.html"),
+  new Route("/services", "Nos services", "/pages/services.html"),
+  new Route("/habitats", "Nos habitats", "/pages/habitats.html"),
+  new Route("/contact", "Contactez nous", "/pages/contact.html"),
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Arcadia";
