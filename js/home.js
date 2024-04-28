07579fdba6e1aead3ab2342dcbb7848a2bdf00c0
SoufianeHakkou <soufiane802@gmail.com>
2024-04-28 15:38:34 +0200

    before changing carousel logic

--- js/home.js
+++ js/home.js
@@ -17,3 +17,24 @@ function toggleAccordion() {
 items.forEach((item) => item.addEventListener("click", toggleAccordion));
 
 // Section 3
+
+let slideIndex = 1;
+
+function changeSlide(n) {
+  showSlides((slideIndex += n));
+}
+
+function showSlides(n) {
+  const slides = document.querySelectorAll(".carousel img");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
+showSlides(slideIndex);
