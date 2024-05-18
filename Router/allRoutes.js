f3d2bf6ed6d0679664adf6c21e6b9669914c6b12
SoufianeHakkou <soufiane802@gmail.com>
2024-05-18 21:29:07 +0200

    Creation dashboard et script.js

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,12 +2,34 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-  new Route("/", "Accueil", "/pages/home.html", "/js/home.js"),
-  new Route("/services", "Nos services", "/pages/services.html"),
-  new Route("/habitats", "Nos habitats", "/pages/habitats.html"),
-  new Route("/connection", "Connexion", "/pages/connection.html"),
-  new Route("/contact", "Contactez nous", "/pages/contact.html"),
+  new Route("/", "Accueil", "/pages/home.html", [], "/js/home.js"),
+  new Route("/services", "Nos services", "/pages/services.html", []),
+  new Route("/habitats", "Nos habitats", "/pages/habitats.html", []),
+  new Route(
+    "/login",
+    "Connexion",
+    "/pages/login.html",
+    ["disconnected"],
+    "/js/login.js"
+  ),
+  new Route("/contact", "Contactez nous", "/pages/contact.html", []),
   new Route("/reservation", "Réservation", "/pages/reservation.html"),
+  new Route(
+    "/admindash",
+    "Espace admin",
+    "/pages/admindashboard.html",
+    ["admin"],
+    "/js/admindashboard.js"
+  ),
+  new Route(
+    "/employeedash",
+    "Espace employé",
+    "/pages/employeedashboard.html",
+    ["employee"]
+  ),
+  new Route("/vetdash", "Espace veterinaire", "/pages/vetdashboard.html", [
+    "vet",
+  ]),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
